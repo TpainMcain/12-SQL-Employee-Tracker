7822e031712eaa04d11013493438a7bf939f5514
TpainMcain <trevorwp77@gmail.com>
2023-05-25 20:40:35 -0500

    rework connection.js to work w env

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,23 @@
-// Import the mysql2 package. This package is a MySQL client for Node.js with support for server-side prepared statements.
+// Import mysql2 package
 const mysql = require('mysql2');
 
-// Create a connection to the database using the 'createConnection' function
-// The function accepts a configuration object which includes the database 
-// details such as host, user, password and database name
+// Import dotenv package to manage environment variables
+require('dotenv').config();
+
+// Create a connection to the database
 const db = mysql.createConnection({
-    // host: The hostname of the database you are connecting to.
-    host: 'localhost',
+    // Host name, typically 'localhost' for local development
+    host: process.env.DB_HOST,
 
-    // user: The MySQL user to authenticate as.
-    user: 'root',
+    // Your MySQL username, stored as an environment variable
+    user: process.env.DB_USER,
 
-    // password: The password of that MySQL user.
-    password: 'zxcqweasd123!!',
+    // Your MySQL password, stored as an environment variable
+    password: process.env.DB_PASSWORD,
 
-    // database: The name of the database to use.
-    database: 'employee_tracker_db'
+    // The name of the database you're connecting to
+    database: process.env.DB_NAME
 });
 
-// Expose the database connection object to be imported by other modules.
-// This allows us to maintain a single connection object throughout our application.
+// Export the connection for use in other files
 module.exports = db;
